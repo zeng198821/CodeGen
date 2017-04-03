a20848f08ddfc4f7e2f35b2caeddd0d54b54889f
zeng <zeng198821@126.com>
2017-04-03 16:47:28 +0800

    Init

---
+++ src/com/yunjuanyunshu/codeGen/action/MainAction.java
@@ -0,0 +1,11 @@
+/**
+ * @File MainAction.java
+ * @author zeng
+ * @Date 2017-04-03
+ * @Time 16:46
+ */
+
+package com.yunjuanyunshu.codeGen.action;
+
+public class MainAction {
+}
---
+++ src/com/yunjuanyunshu/codeGen/ui/MainForm.java
@@ -0,0 +1,11 @@
+/**
+ * @File MainForm.java
+ * @author zeng
+ * @Date 2017-04-03
+ * @Time 16:47
+ */
+
+package com.yunjuanyunshu.codeGen.ui;
+
+public class MainForm {
+}
